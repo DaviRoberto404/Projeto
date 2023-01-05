5bc708ecb12427a42c163a2d8c0a732e878f10cf
DaviRoberto404 <davi44509@gmail.com>
2023-01-05 16:10:10 -0300

    Criando algoritmos de frases aleatorias

--- frase2.js
+++ frase2.js
@@ -4,6 +4,17 @@ var frases = ['Ola bom dia.',
                 'O ser humano tem 70 mil pensamentos por dia.',
                     'Tartaruga come pombo.',
                         'Djoga lança album sempre dia 13',
-                            'Ladrão roubou pra comprar cervejinha',
-                                'Seja selvagem e livre jovem!',
+                            'Ladrão roubou pra comprar cervejinha confia bro',
+                                'Seja selvagem e livre meu jovem!',
                                     'Sucesso após ver o progresso']
+
+function mudaFrase(){
+
+    var frase = $('.frase');
+    var numAleatorio = Math.floor(Math.random() * frases.length)
+
+    frase.text(frases[numAleatorio]);
+
+    atualizaPalavras();
+
+}
